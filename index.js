539382bfd6dbff67f63b5fc8132cd9da7a8e7bfb
jibblol <jibblol@gmail.com>
2017-05-28 17:09:14 +0200

    simplify

--- index.js
+++ index.js
@@ -9,31 +9,15 @@ var db = {
         bonuspoints: 2000
     }],
     bookings: [{
-        booking_id: 0,
-        user_id: 0,
         startDate: "2017-05-10",
         endDate: "2017-05-17",
         room: "Standard",
-        hotel: {
-        name: "Fawlty Towers",
-            map: {
-                lat: "-43.9509",
-                lng: "-34.4618"
-            }
-        }
+        hotel: "Fawlty Towers"
     }, {
-        booking_id: 1,
-        user_id: 0,
         startDate: "2017-05-25",
         endDate: "2017-05-29",
         room: "Deluxe",
-        hotel: {
-            name: "Fawlty Towers",
-            map: {
-                lat: "-43.9509",
-                lng: "-34.4618"
-            }
-        }
+        hotel: "Fawlty Towers"
     }]
 }
 var router = jsonServer.router(db)
